0f3f828db44b3c6771776e4a8680f85aa8c1d9c7
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-02-27 20:38:22 -0800

    Manage the login

--- web/src/Pages/Dashboard.tsx
+++ web/src/Pages/Dashboard.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { PieChart, Pie, Cell } from 'recharts';
+
 import AuthButton from '../components/AuthButton';
 import AuthInput from '../components/AuthInput';
+import AuthTitle from '../components/AuthTitle';
 import HistoryBox from '../components/HistoryBox';
 import Label from '../components/Label';
 
@@ -12,115 +16,144 @@ const data = [
 const colors = ['#f3ef52', '#DC2626'];
 
 export default function Dashboard() {
+  const userToken = localStorage.getItem('user-token');
+
   return (
-    <div className="bg-primary-500 mx-auto max-w-2xl md:max-w-4xl lg:max-w-7xl px-6 lg:px-8 mt-24 mb-24">
-      <div className="grid grid-cols-12 gap-0">
-        <div className="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 xxl:col-span-4">
-          <div>
-            <div className="px-8 py-2">
-              <h4 className="text-lg text-gray-400 font-thin">Your Balance</h4>
-              <h4 className="text-2xl font-semibold">CAD$ 430.00</h4>
-            </div>
+    <>
+      {userToken ? (
+        <div className="bg-primary-500 mx-auto max-w-2xl md:max-w-4xl lg:max-w-7xl px-6 lg:px-8 mt-24 mb-24">
+          <div className="grid grid-cols-12 gap-0">
+            <div className="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-4 xxl:col-span-4">
+              <div>
+                <div className="px-8 py-2">
+                  <h4 className="text-lg text-gray-400 font-thin">
+                    Your Balance
+                  </h4>
+                  <h4 className="text-2xl font-semibold">CAD$ 430.00</h4>
+                </div>
 
-            <div className="flex space-x-0 flex-col lg:flex-row lg:space-x-2 my-2 px-6">
-              <div className="bg-accent-500 p-4 rounded-md shadow-lg w-full text-secondary-500 text-center">
-                <h1 className="text-xl font-light">INCOME</h1>
-                <h1 className="text-2xl text-primary-500 font-semibold">
-                  $ 6000.00
-                </h1>
-              </div>
-
-              <div className="bg-red-600 p-4 rounded-md shadow-lg w-full text-secondary-500 text-center">
-                <h1 className="text-xl font-light">EXPENSE</h1>
-                <h1 className="text-2xl text-primary-500 font-semibold">
-                  $ 170.00
-                </h1>
-              </div>
-            </div>
-
-            <div className="px-8 my-6">
-              <div className="my-4 border-b w-full">
-                <h2 className="font-semibold text-lg">History</h2>
-              </div>
-
-              {/* Change for a map with the api data */}
-              <HistoryBox text="My wallet" value={400} />
-              <HistoryBox text="My wallet" value={-400} />
-              <HistoryBox text="My wallet" value={400} />
-            </div>
-
-            <div className="px-8 my-6">
-              <div className="my-4 border-b w-full">
-                <h2 className="font-semibold text-lg">Add new transaction</h2>
-              </div>
-
-              <div className="bg-primary-500 border-2 border-secondary-500 p-4 rounded-md">
-                <form className="mt-4">
-                  <div className="my-5 text-sm">
-                    <Label htmlFor="text" content="Text" />
-                    <AuthInput
-                      name="text"
-                      id="text"
-                      autoFocus
-                      placeholder="Enter Text"
-                    />
+                <div className="flex space-x-0 flex-col lg:flex-row lg:space-x-2 my-2 px-6">
+                  <div className="bg-accent-500 p-4 rounded-md shadow-lg w-full text-secondary-500 text-center">
+                    <h1 className="text-xl font-light">INCOME</h1>
+                    <h1 className="text-2xl text-primary-500 font-semibold">
+                      $ 6000.00
+                    </h1>
                   </div>
 
-                  <div className="my-5 text-sm">
-                    <label
-                      htmlFor="amount"
-                      className="block mb-2 text-sm font-medium"
-                    >
-                      Amount
-                      <small className="text-white">
-                        {' '}
-                        (<span className="text-red-400">
-                          {' '}
-                          negative-expense
-                        </span>{' '}
-                        ,
-                        <span className="text-accent-500">
-                          {' '}
-                          positive-income
-                        </span>
-                        )
-                      </small>
-                    </label>
-                    <AuthInput
-                      name="amount"
-                      id="amount"
-                      autoFocus
-                      placeholder="Enter Amount"
-                    />
+                  <div className="bg-red-600 p-4 rounded-md shadow-lg w-full text-secondary-500 text-center">
+                    <h1 className="text-xl font-light">EXPENSE</h1>
+                    <h1 className="text-2xl text-primary-500 font-semibold">
+                      $ 170.00
+                    </h1>
+                  </div>
+                </div>
+
+                <div className="px-8 my-6">
+                  <div className="my-4 border-b w-full">
+                    <h2 className="font-semibold text-lg">History</h2>
                   </div>
 
-                  <div className="my-5">
-                    <AuthButton title="Add Transaction" />
+                  {/* Change for a map with the api data */}
+                  <HistoryBox text="My wallet" value={400} />
+                  <HistoryBox text="My wallet" value={-400} />
+                  <HistoryBox text="My wallet" value={400} />
+                </div>
+
+                <div className="px-8 my-6">
+                  <div className="my-4 border-b w-full">
+                    <h2 className="font-semibold text-lg">
+                      Add new transaction
+                    </h2>
                   </div>
-                </form>
+
+                  <div className="bg-primary-500 border-2 border-secondary-500 p-4 rounded-md">
+                    <form className="mt-4">
+                      <div className="my-5 text-sm">
+                        <Label htmlFor="text" content="Text" />
+                        <AuthInput
+                          name="text"
+                          id="text"
+                          autoFocus
+                          placeholder="Enter Text"
+                        />
+                      </div>
+
+                      <div className="my-5 text-sm">
+                        <label
+                          htmlFor="amount"
+                          className="block mb-2 text-sm font-medium"
+                        >
+                          Amount
+                          <small className="text-white">
+                            {' '}
+                            (
+                            <span className="text-red-400">
+                              {' '}
+                              negative-expense
+                            </span>{' '}
+                            ,
+                            <span className="text-accent-500">
+                              {' '}
+                              positive-income
+                            </span>
+                            )
+                          </small>
+                        </label>
+                        <AuthInput
+                          name="amount"
+                          id="amount"
+                          autoFocus
+                          placeholder="Enter Amount"
+                        />
+                      </div>
+
+                      <div className="my-5">
+                        <AuthButton title="Add Transaction" />
+                      </div>
+                    </form>
+                  </div>
+                </div>
               </div>
+            </div>
+
+            <div className="w-full flex items-center justify-center col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-8 xxl:col-span-4">
+              <PieChart width={500} height={700}>
+                <Pie
+                  className="focus:outline-none"
+                  data={data}
+                  dataKey="amount"
+                  outerRadius={150}
+                  innerRadius={100}
+                  nameKey="text"
+                  label
+                  stroke="#27292f"
+                >
+                  {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={colors[index]} />
+                  ))}
+                </Pie>
+              </PieChart>
             </div>
           </div>
         </div>
+      ) : (
+        <section className="bg-primary-500">
+          <div className="flex flex-col items-center justify-center px-6 py-16 mx-auto md:h-screen lg:py-0">
+            <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-primary-500 border-secondary-500">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <AuthTitle content="Sorry! You need to be logged in to see this page content" />
 
-        <div className="w-full flex items-center justify-center col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-8 xxl:col-span-4">
-          <PieChart width={500} height={700}>
-            <Pie
-              data={data}
-              dataKey="amount"
-              outerRadius={150}
-              innerRadius={100}
-              nameKey="text"
-              label
-              stroke="#27292f"
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={colors[index]} />
-              ))}
-            </Pie>
-          </PieChart>
-        </div>
-      </div>
-    </div>
+                <Link
+                  to="/login"
+                  className="block w-full bg-secondary-500 hover:bg-accent-500 hover:text-primary-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-accent-500"
+                >
+                  Sign In
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
--- web/src/Pages/Login.tsx
+++ web/src/Pages/Login.tsx
@@ -1,3 +1,7 @@
+import { FormEvent, useState } from 'react';
+import { api } from '../lib/axios';
+import { useNavigate } from 'react-router-dom';
+
 import AuthInput from '../components/AuthInput';
 import Label from '../components/Label';
 import AuthButton from '../components/AuthButton';
@@ -6,6 +10,42 @@ import AuthTitle from '../components/AuthTitle';
 import AuthParagraph from '../components/AuthParagraph';
 
 export default function Login() {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    if (!loginEmail || !loginPassword) {
+      return;
+    }
+
+    api
+      .post('/api/users/login', {
+        email: loginEmail,
+        password: loginPassword
+      })
+      .then(response => {
+        const token = response.data.accesToken;
+
+        if (!token) {
+          alert('Unable to login. Please try again!');
+        }
+
+        // Store the user token on the local storage of the browser
+        localStorage.clear();
+        localStorage.setItem('user-token', token);
+
+        setTimeout(() => {
+          navigate('/dashboard');
+        }, 500);
+      })
+      .catch(error => {
+        alert('Oops! Some error occured, please try again');
+      });
+  }
+
   return (
     <section className="bg-primary-500">
       <div className="flex flex-col items-center justify-center px-6 py-16 mx-auto md:h-screen lg:py-0">
@@ -15,14 +55,16 @@ export default function Login() {
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <AuthTitle content="Sign in to your account" />
 
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form onSubmit={handleLogin} className="space-y-4 md:space-y-6">
               <div>
                 <Label htmlFor="loginEmail" content="Your email" />
                 <AuthInput
                   type="email"
-                  name="loginEmail"
                   id="loginEmail"
                   placeholder="name@company.com"
+                  value={loginEmail}
+                  onChange={event => setLoginEmail(event.target.value)}
+                  autoFocus
                   required
                 />
               </div>
@@ -31,9 +73,10 @@ export default function Login() {
                 <Label htmlFor="loginPassword" content="Password" />
                 <AuthInput
                   type="password"
-                  name="loginPassword"
                   id="loginPassword"
                   placeholder="••••••••"
+                  value={loginPassword}
+                  onChange={event => setLoginPassword(event.target.value)}
                   required
                 />
               </div>

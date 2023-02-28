69b7da6eb56aafa5b193c1eeb602921b7373b9c4
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-02-28 00:50:05 -0800

    Create update dialog

--- web/src/components/AuthInput.tsx
+++ web/src/components/AuthInput.tsx
@@ -6,7 +6,7 @@ export default function AuthInput(props: InputProps) {
   return (
     <input
       {...props}
-      className="border border-secondary-500 sm:text-sm rounded-lg block w-full p-2.5 bg-secondary-500 placeholder-gray-400 focus:ring-accent-500 focus:ring-4 focus:outline-none"
+      className="border border-secondary-500 sm:text-sm rounded-lg block w-full p-2.5 bg-secondary-500 placeholder-gray-400 focus:ring-accent-500 focus:ring-2 focus:outline-none"
     />
   );
 }
--- web/src/components/TableRow.tsx
+++ web/src/components/TableRow.tsx
@@ -1,3 +1,11 @@
+import { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { XMarkIcon } from '@heroicons/react/24/outline';
+
+import Label from './Label';
+import AuthInput from './AuthInput';
+import AuthButton from './AuthButton';
+
 interface TableRowProps {
   text: string;
   value: number;
@@ -6,6 +14,9 @@ interface TableRowProps {
 }
 
 export default function TableRow(props: TableRowProps) {
+  const [updateText, setUpdateText] = useState('');
+  const [updateValue, setUpdateValue] = useState('');
+
   async function handleClick() {
     props.onDelete(props.id);
   }
@@ -19,7 +30,52 @@ export default function TableRow(props: TableRowProps) {
         {props.value}
       </td>
       <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-        <button className="text-accent-500 hover:opacity-90">Edit</button>
+        <Dialog.Root>
+          <Dialog.Trigger
+            type="button"
+            className="text-accent-500 hover:opacity-90"
+          >
+            Edit
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+
+            <Dialog.Content className="absolute p-10 mt-32 bg-primary-500 border border-accent-500 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              <Dialog.Close className="absolute right-6 top-6 text-gray-400 rounded-lg hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-accent-500 focus:ring-offset-2 focus:ring-offset-primary-500">
+                <XMarkIcon className="block h-6 w-6" />
+              </Dialog.Close>
+
+              <Dialog.Title className="text-3xl leading-tight font-bold">
+                Update Transaction
+              </Dialog.Title>
+
+              <div className="mt-8">
+                <Label htmlFor="updateValue" content="New Value" />
+                <AuthInput
+                  id="updateValue"
+                  placeholder="New Value"
+                  value={updateValue}
+                  onChange={event => setUpdateValue(event.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="mt-8 mb-8">
+                <Label htmlFor="updateText" content="New Text" />
+                <AuthInput
+                  id="updateText"
+                  placeholder="New Text"
+                  value={updateText}
+                  onChange={event => setUpdateText(event.target.value)}
+                  required
+                />
+              </div>
+
+              <AuthButton title="Update" />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </td>
       <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
         <button

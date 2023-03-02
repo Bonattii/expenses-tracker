c216958e0eff4552c2299f0393820c4fd89eb6f2
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-03-01 17:06:48 -0800

    Change about photo

--- web/src/components/About.tsx
+++ web/src/components/About.tsx
@@ -12,7 +12,7 @@ export default function About() {
       className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32"
     >
       <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        src="https://images.unsplash.com/photo-1569025743873-ea3a9ade89f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
         alt="People working in a company environment"
         className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
       />
@@ -35,7 +35,7 @@ export default function About() {
             y2="363.075"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stopColor="rgb(209 213 219)" />
+            <stop stopColor="#141518" />
             <stop offset={1} stopColor="#f3ef52" />
           </linearGradient>
         </defs>
